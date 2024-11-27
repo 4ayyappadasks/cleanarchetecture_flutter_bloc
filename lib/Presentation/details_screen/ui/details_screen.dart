63ad75f5f26955794b6f3719ed1a2b3de4df1fa6
Ayyapadas <ayyappadas@auxzon.com>
2024-11-27 11:19:37 +0530

    commit

--- lib/Presentation/details_screen/ui/details_screen.dart
+++ lib/Presentation/details_screen/ui/details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cleanarchetectureflutterguy/Presentation/details_screen/bloc/details_screen_bloc.dart';
 import 'package:cleanarchetectureflutterguy/Presentation/home_screen/ui/homepage.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +13,10 @@ class DetailsScreenWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
-      DetailsScreenBloc()
-        ..add(Detailsscreenloadevent(id: id)),
-      child: DetailsScreen(id: id,),
+          DetailsScreenBloc()..add(Detailsscreenloadevent(id: id)),
+      child: DetailsScreen(
+        id: id,
+      ),
     );
   }
 }
@@ -28,26 +30,27 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: () {
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const Homepagewrappwer(),));
-        }, icon: const Icon(Icons.arrow_back_outlined)),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const Homepagewrappwer(),
+              ));
+            },
+            icon: const Icon(Icons.arrow_back_outlined)),
         centerTitle: true,
         title: BlocBuilder<DetailsScreenBloc, DetailsScreenState>(
           builder: (context, state) {
             if (state is DetailsScreenInitial) {
               return const Text("Details Screen");
-            }
-            else if (state is DetailsScreenLoading) {
-              return const LinearProgressIndicator(color: Colors.green,);
-            }
-            else if (state is DetailsScreenLoaded) {
+            } else if (state is DetailsScreenLoading) {
+              return const LinearProgressIndicator(
+                color: Colors.green,
+              );
+            } else if (state is DetailsScreenLoaded) {
               return Text(state.detailsEntity.title);
-            }
-            else if (state is DetailsScreenError) {
+            } else if (state is DetailsScreenError) {
               return const Text("Details Screen");
-            }
-            else {
+            } else {
               return const Text("Details Screen");
             }
           },
@@ -57,17 +60,33 @@ class DetailsScreen extends StatelessWidget {
         builder: (context, state) {
           if (state is DetailsScreenInitial) {
             return const Text("waiting");
-          }
-          else if (state is DetailsScreenLoading) {
-            return const Center(child: CircularProgressIndicator(color: Colors.green,));
-          }
-          else if (state is DetailsScreenLoaded) {
-            return Center(child: Text(state.detailsEntity.title));
-          }
-          else if (state is DetailsScreenError) {
+          } else if (state is DetailsScreenLoading) {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: Colors.green,
+            ));
+          } else if (state is DetailsScreenLoaded) {
+            return Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CachedNetworkImage(imageUrl: state.detailsEntity.images[0]),
+                  Text(
+                    state.detailsEntity.description,
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    "price : ${state.detailsEntity.price.toString()}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ));
+          } else if (state is DetailsScreenError) {
             return const Center(child: Text("data is error"));
-          }
-          else {
+          } else {
             return const Center(child: Text("error in loading"));
           }
         },
--- lib/Presentation/home_screen/ui/homepage.dart
+++ lib/Presentation/home_screen/ui/homepage.dart
@@ -1,7 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cleanarchetectureflutterguy/Common/authservice/authservice.dart';
 import 'package:cleanarchetectureflutterguy/Presentation/details_screen/ui/details_screen.dart';
 import 'package:cleanarchetectureflutterguy/Presentation/home_screen/widgets/topwidgets/bloc/topwidgets_bloc.dart';
 import 'package:cleanarchetectureflutterguy/Presentation/home_screen/widgets/topwidgets/ui/topwidget.dart';
+import 'package:cleanarchetectureflutterguy/Presentation/spalsh_screen/ui/splash_screen_ui.dart';
+import 'package:cleanarchetectureflutterguy/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/homepage_bloc.dart';
@@ -14,10 +17,14 @@ class Homepagewrappwer extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => HomepageBloc()..add(Loadhomedata()),
+          create: (context) =>
+          HomepageBloc()
+            ..add(Loadhomedata()),
         ),
         BlocProvider(
-          create: (context) => TopwidgetsBloc()..add(TopwidgetEvents()),
+          create: (context) =>
+          TopwidgetsBloc()
+            ..add(TopwidgetEvents()),
         ),
       ],
       child: const Homepage(),
@@ -31,88 +38,113 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: RefreshIndicator(
-          onRefresh: () async {
-           context.read<HomepageBloc>().add(Loadhomedata());
-           context.read<TopwidgetsBloc>().add(TopwidgetEvents());
-          },
-          child: Center(
-            child: BlocBuilder<HomepageBloc, HomepageState>(
-              builder: (context, state) {
-                if (state is HomepageInitial) {
-                  return Text("waiting");
-                } else if (state is HomepageLoading) {
-                  return CircularProgressIndicator(
-                    color: Colors.red,
-                  );
-                } else if (state is HomepageLoaded) {
-                  return Column(
-                    children: [
-                      const Topwidget(),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Expanded(
-                        child: GridView.builder(
-                          physics: BouncingScrollPhysics(),
-                          itemCount: state.homepageentity.products.length,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount:3,
-                            childAspectRatio: .7,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10,
-                          ),
-                          itemBuilder: (context, index) {
-                            final imageUrl =
-                                state.homepageentity.products[index].images[0];
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => DetailsScreenWrapper(id: "${state.homepageentity.products[index].id}",),));
-                              },
-                              child: Material(
-                                elevation: 5,
-                                borderRadius: BorderRadius.circular(5),
-                                child: Column(
-                                  children: [
-                                    CachedNetworkImage(
-                                      imageUrl: imageUrl,
-                                      fit: BoxFit.contain,
-                                      progressIndicatorBuilder:
-                                          (context, url, downloadProgress) {
-                                        final progress =
-                                            downloadProgress.progress ?? 0.0;
-                                        return Center(
-                                          child: CircularProgressIndicator(
-                                              value: progress),
-                                        );
-                                      },
-                                      errorWidget: (context, url, error) {
-                                        return const Center(
-                                          child: Icon(Icons.error, color: Colors.red),
-                                        );
-                                      },
-                                    ),
-                                    Expanded(
-                                        child: Text(state
-                                            .homepageentity.products[index].title,textAlign: TextAlign.center,
-                                        overflow: TextOverflow.ellipsis,maxLines: 2,))
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
+      child: PopScope(
+        canPop: false,
+        child: Scaffold(
+          appBar: AppBar(
+            forceMaterialTransparency: true,
+            automaticallyImplyLeading: false,
+            centerTitle: true,
+            title: const Text("Flutter Bloc"),
+            actions: [
+              IconButton(onPressed: () {
+                AuthService.clearTokens().then((value) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => SplashScreenUiWrapper(),));
+                },);
+              }, icon: Icon(Icons.logout))
+            ],
+          ),
+          body: RefreshIndicator(
+            onRefresh: () async {
+              context.read<HomepageBloc>().add(Loadhomedata());
+              context.read<TopwidgetsBloc>().add(TopwidgetEvents());
+            },
+            child: Center(
+              child: BlocBuilder<HomepageBloc, HomepageState>(
+                builder: (context, state) {
+                  if (state is HomepageInitial) {
+                    return Text("waiting");
+                  } else if (state is HomepageLoading) {
+                    return CircularProgressIndicator(
+                      color: Colors.red,
+                    );
+                  } else if (state is HomepageLoaded) {
+                    return Column(
+                      children: [
+                        const Topwidget(),
+                        const SizedBox(
+                          height: 20,
                         ),
-                      ),
-                    ],
-                  );
-                } else if (state is HomepageError) {
-                  return Text("error ${state.errormsg}");
-                } else {
-                  return const Text("error");
-                }
-              },
+                        Expanded(
+                          child: GridView.builder(
+                            physics: BouncingScrollPhysics(),
+                            itemCount: state.homepageentity.products.length,
+                            gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: .7,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                            ),
+                            itemBuilder: (context, index) {
+                              final imageUrl =
+                              state.homepageentity.products[index].images[0];
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).pushReplacement(
+                                      MaterialPageRoute(builder: (context) =>
+                                          DetailsScreenWrapper(
+                                            id: "${state.homepageentity
+                                                .products[index].id}",),));
+                                },
+                                child: Material(
+                                  elevation: 5,
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Column(
+                                    children: [
+                                      CachedNetworkImage(
+                                        imageUrl: imageUrl,
+                                        fit: BoxFit.contain,
+                                        progressIndicatorBuilder:
+                                            (context, url, downloadProgress) {
+                                          final progress =
+                                              downloadProgress.progress ?? 0.0;
+                                          return Center(
+                                            child: CircularProgressIndicator(
+                                                value: progress),
+                                          );
+                                        },
+                                        errorWidget: (context, url, error) {
+                                          return const Center(
+                                            child: Icon(
+                                                Icons.error, color: Colors.red),
+                                          );
+                                        },
+                                      ),
+                                      Expanded(
+                                          child: Text(state
+                                              .homepageentity.products[index]
+                                              .title,
+                                            textAlign: TextAlign.center,
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 2,))
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  } else if (state is HomepageError) {
+                    return Text("error ${state.errormsg}");
+                  } else {
+                    return const Text("error");
+                  }
+                },
+              ),
             ),
           ),
         ),
